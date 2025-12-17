ce48dfdbee461aff0cfc69a78f88f26456dfc38b
Mikael Melo <mikaelsilva1940@hotmail.com>
2025-12-17 09:43:44 -0300

    feat: classe posição

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tabuleiro;
 
 namespace xadrez
 {
@@ -10,6 +11,9 @@ namespace xadrez
     {
         static void Main(string[] args)
         {
+
+            Posicao p = new Posicao(3, 4);
+            Console.WriteLine("Posição" +p);
         }
     }
 }

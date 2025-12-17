7fc5e2d603f9636b4755b308704d62a6b5258c30
Mikael Melo <mikaelsilva1940@hotmail.com>
2025-12-17 14:11:05 -0300

    feat: ajuste na implementação método imprimir

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -21,8 +21,8 @@ namespace xadrez
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
                 tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
-
+              
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(5, 5));
 
 
                 Tela.imprimirTabuleiro(tab);

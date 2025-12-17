512e69047fb487973f0c54fb570a7bdf8caddb63
Mikael Melo <mikaelsilva1940@hotmail.com>
2025-12-17 20:55:02 -0300

    feat: controlando peças capturadas

--- xadrez/Tela.cs
+++ xadrez/Tela.cs
@@ -1,10 +1,45 @@
 ﻿using tabuleiro;
 using System;
+using System.Collections.Generic;
 
 namespace xadrez
 {
     internal class Tela
     {
+
+        public static void imprimirPartida(PartidadeXadrez partida)
+        {
+            Tela.imprimirTabuleiro(partida.tab);
+            Console.WriteLine();
+            imprimirPecasCapturadas(partida);
+            Console.WriteLine();
+            Console.WriteLine("Turno: " + partida.turno);
+            Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+        }
+
+        public static void imprimirPecasCapturadas (PartidadeXadrez partida)
+        {
+            Console.WriteLine("Peças capturadas: ");
+            Console.WriteLine("Brancas: ");
+            imprimirConjunto(partida.pecasCapturadas(Cor.Branca));
+            Console.WriteLine("Pretas: ");
+            ConsoleColor aux = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            imprimirConjunto(partida.pecasCapturadas(Cor.Preta));
+            Console.ForegroundColor= aux;
+            Console.WriteLine();
+        }
+
+        public static void imprimirConjunto(HashSet<Peca> conjunto)
+        {
+            Console.Write("[");
+            foreach (Peca x in conjunto )
+            {
+                Console.Write(x + " ");
+            }
+            Console.WriteLine("]");
+        }
+
         public static void imprimirTabuleiro(Tabuleiro tab)
         {
             for (int i = 0; i < tab.linhas; i++)
--- xadrez/xadrez/PosicaoXadrez.cs
+++ xadrez/xadrez/PosicaoXadrez.cs
@@ -13,6 +13,12 @@ namespace xadrez
         public char coluna {  get; set; }
         public int linha { get; set; }
 
+
+        public PosicaoXadrez ()
+        {
+
+        }
+
         public PosicaoXadrez(char coluna, int linha)
         {
             this.coluna = coluna;
